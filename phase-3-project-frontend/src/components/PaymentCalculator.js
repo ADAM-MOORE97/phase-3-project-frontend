06f3c4847c6baded0c17b347ae1e1bfd7e298945
Amy Posten <amyposten85@gmail.com>
2021-11-16 10:36:08 -0500

    Form submit for calculator

--- phase-3-project-frontend/src/components/PaymentCalculator.js
+++ phase-3-project-frontend/src/components/PaymentCalculator.js
@@ -1,12 +1,58 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const PaymentCalculator = () => {
+  const [userInput, setUserInput] = useState({
+    amount: "",
+    term: "",
+    interest: "",
+    payment: "",
+  });
+
+  const handleInput = (e) =>
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInput);
+  };
+
   return (
     <>
-      <p><input placeholder="loan amount" /></p>
-      <p><input placeholder="term" /></p>
-      <p><input placeholder="Interest Rate %" /></p>
-      <button>calculate</button>
+      <p>
+        <input
+          placeholder="loan amount"
+          value={userInput.amount}
+          onChange={handleInput}
+        />
+      </p>
+      <p>
+        <input
+          placeholder="term"
+          value={userInput.term}
+          onChange={handleInput}
+        />
+      </p>
+      months
+      <label className="switch">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+      </label>
+      years
+      <p>
+        <input
+          placeholder="Interest Rate %"
+          value={userInput.interest}
+          onChange={handleInput}
+        />
+      </p>
+      <p>
+        <input
+          placeholder="monthly payment"
+          value={userInput.payment}
+          onChange={handleInput}
+        />
+      </p>
+      <button onSubmit={handleSubmit}>calculate</button>
     </>
   );
 };
