101a68260d50daaa21e5b1d2cf5cc7e49842784c
Amy Posten <amyposten85@gmail.com>
2021-11-16 09:38:55 -0500

    Calculator buttons and inputs

--- phase-3-project-frontend/src/App.js
+++ phase-3-project-frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import PaymentCalculator from './components/PaymentCalculator';
 
 function App() {
   return (
     <div className="App">
-  
+      <PaymentCalculator />
     </div>
   );
 }
--- phase-3-project-frontend/src/components/PaymentCalculator.js
+++ phase-3-project-frontend/src/components/PaymentCalculator.js
@@ -1,5 +1,14 @@
 import { useState, useEffect } from "react";
 
 const PaymentCalculator = () => {
-  return <></>;
+  return (
+    <>
+      <p><input placeholder="loan amount" /></p>
+      <p><input placeholder="term" /></p>
+      <p><input placeholder="Interest Rate %" /></p>
+      <button>calculate</button>
+    </>
+  );
 };
+
+export default PaymentCalculator;
