7b2816258bee8d89b14c81f4afd0dbab8301b98b
Adam <adammoore118@outlook.com>
2021-11-17 10:35:07 -0500

    initial navbar set up

---
+++ phase-3-project-frontend/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+const Navbar = () =>{
+const [display, setDisplay] =useState(false)
+
+    return(
+        <div style={{display: 'inline'}}>
+        <button>Home</button>
+        <button>About</button>
+        <button>FAQs</button>
+        <div style={{display: 'inline'}} className="subnav">
+            <button onClick={(e)=>setDisplay(!display)}  className="subnavbtn">"UserName"▼</button>
+            <div className="subnav-content">
+                <a  style={display?{display: 'block'}:{display: 'none'}} href="">Profile</a>
+                <a  style={display?{display: 'block'}:{display: 'none'}} href="">Log Out</a>
+            </div>
+        </div>
+        
+
+        </div>
+    )
+}
+
+export default Navbar;
