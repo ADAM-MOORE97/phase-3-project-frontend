0ce5e7b89d775075373de4312d4888bc821778e9
Amy Posten <amyposten85@gmail.com>
2021-11-18 17:44:00 -0500

    Landing page css

--- phase-3-project-frontend/src/components/LandingPage.js
+++ phase-3-project-frontend/src/components/LandingPage.js
@@ -1,45 +1,77 @@
-import {useState, useEffect} from 'react'
-import {Link} from "react-router-dom";
-import Loanlist from './Loanlist';
-import {useHistory} from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Loanlist from "./Loanlist";
+import { useHistory } from "react-router-dom";
 
-const LandingPage = ({people,setPersonState,loans, postPayment}) => {
-    const [login, setLogIn]=useState({username: '',password: ''})
-    const [verified, setVerified] = useState(true)
-    let history = useHistory();
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        people.filter(person => {
-            if(person.username === login.username && person.password === login.password) {
-                setPersonState(person);
-                console.log(person)
-                history.push('/home')
-            }
-            else if(person.username !== login.username && person.password !== login.password){
-                setVerified(!verified)
-            }
-        });
-        e.target.reset()
-    }
- 
-  
-    return(
-        <>
-            <h1 className="title">Loan Managizer</h1>
+const LandingPage = ({ people, setPersonState, loans, postPayment }) => {
+  const [login, setLogIn] = useState({ username: "", password: "" });
+  const [verified, setVerified] = useState(true);
+  let history = useHistory();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    people.filter((person) => {
+      if (
+        person.username === login.username &&
+        person.password === login.password
+      ) {
+        setPersonState(person);
+        console.log(person);
+        history.push("/home");
+      } else if (
+        person.username !== login.username &&
+        person.password !== login.password
+      ) {
+        setVerified(!verified);
+      }
+    });
+    e.target.reset();
+  };
 
-            <h3>Welcome Message ......</h3>
-            {verified? <p>Please LOGIN, new user's can signup by clicking button below!</p>:<p>Uh oh! user not recognized, try again or signup below!</p>}
-            <form onSubmit={handleSubmit}>
-            <p><input onChange={e=>setLogIn({...login, username:e.target.value})} placeholder='username'></input></p>
-            <p><input type="password" onChange={e=>setLogIn({...login, password:e.target.value})} placeholder='password'></input></p>
-            <button type="submit">Log In</button>
-            </form>
-            
-            <Link to='/signup'><button>Signup</button></Link>
+  return (
+    <>
+      <h1 className="title">Loan Managizer</h1>
+      <div className="landing">
+        {/* <h3 className="welcome-message">Welcome Message ......</h3> */}
+        {verified ? (
+          <p className="welcome-message">
+            Please LOGIN, new users can signup by clicking the button below!
+          </p>
+        ) : (
+          <p className="welcome-message">
+            Uh oh! user not recognized, try again or signup below!
+          </p>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div className="login">
+            <p>
+              <input
+                onChange={(e) =>
+                  setLogIn({ ...login, username: e.target.value })
+                }
+                placeholder="username"
+              ></input>
+            </p>
+            <p>
+              <input
+                type="password"
+                onChange={(e) =>
+                  setLogIn({ ...login, password: e.target.value })
+                }
+                placeholder="password"
+              ></input>
+            </p>
+          </div>
+          <button type="submit" className="landing-button">
+            Log In
+          </button>
+        </form>
 
-            
-        </>
-    );
-}
+        <Link to="/signup">
+          <button className="landing-button">Signup</button>
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default LandingPage;
