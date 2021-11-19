95382fc699e232e1e788b9552bf644bfb90510d7
Adam <adammoore118@outlook.com>
2021-11-18 20:23:03 -0500

    frontend persists visually through all components.

--- phase-3-project-frontend/src/components/Loanlist.js
+++ phase-3-project-frontend/src/components/Loanlist.js
@@ -1,8 +1,20 @@
 import React, {useState, useEffect} from "react";
 import Loan from "./Loan";
 import { Link } from "react-router-dom";
-function Loanlist({loans, postPayment}) {
-      const singleloan = loans.map((loan)=>{
+function Loanlist({loans,person, postPayment, setPerson}) {
+    const [loanState, setLoanState] = useState(loans)
+   function deleteLoan(loanvalue){
+    if (loanvalue.current_value === 0){
+        fetch(`http://localhost:9292/loans/${loanvalue.id}`, {
+            method: "DELETE",
+            
+          })
+          .then(r=>r.json())
+          .then(data=>setLoanState(data))
+        
+       }
+   }
+      let singleloan = loanState.map((loan)=>{
           return <Loan
           key={loan.id}
           name= {loan.item.name}
@@ -11,14 +23,18 @@ function Loanlist({loans, postPayment}) {
           loan= {loan}
           postPayment={postPayment}
           interest_rate={loan.interest_rate}
+          setPerson={setPerson}
+          person={person}
+          deleteLoan={deleteLoan}
           />
       })
+      
 return(
     <div>
-    { (loans[0]!==undefined)?
+    { (loanState[0]!==undefined)?
     <div>
         <h1>Loan list</h1>
-       <ul>{singleloan}</ul>
+       <div>{singleloan}</div>
     </div>:
     <div>
     <h3>Please log in to see your list of loans!</h3> 
