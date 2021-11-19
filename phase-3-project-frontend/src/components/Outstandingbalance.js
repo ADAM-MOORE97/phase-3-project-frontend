fbfc4d3ea7d8f322b0c4b9c04579f3aecd39d20e
Jane <janechodance@gmail.com>
2021-11-18 23:51:43 -0500

    pie chart

--- phase-3-project-frontend/src/components/Outstandingbalance.js
+++ phase-3-project-frontend/src/components/Outstandingbalance.js
@@ -1,7 +1,9 @@
 import React from "react" 
+import { PieChart, Pie} from 'recharts';
 function Outstandingbalance({loans}){
     let loanArray = []
     loanArray = [...loans.map((loan)=> (loan.current_value))]
+    console.log(loanArray)
     function simpleArraySum(ar) {
         var sum = 0;
         for (var i = 0; i < ar.length; i++) {
@@ -10,10 +12,28 @@ function Outstandingbalance({loans}){
         return sum;
       }
     let ArraySum = simpleArraySum(loanArray)
+    
+      
+    let pieData = []
+    pieData= [...loans.map((loan)=>loan.item.name)]
+    let finalArray = pieData.map(function(x,i){
+      return { item_name: x, value: loanArray[i] }
+    })
+    console.log(finalArray)
+    let renderLabel = function(entry) {
+      return `${entry.item_name}: ${entry.value}`;
+  }
     return(
         <div>
         <h1>You Owe:  {ArraySum} dollars!</h1>
         <h1>Pie chart</h1>
+        <PieChart width={900} height={900}>
+          <Pie data={finalArray} 
+          dataKey="value" 
+          outerRadius={200} 
+          fill="#e9d8a6" 
+          label={renderLabel} />
+        </PieChart>
         </div>
     )
 }
