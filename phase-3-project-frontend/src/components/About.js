18e00431249bd77d93306aebc74241e046312739
Amy Posten <amyposten85@gmail.com>
2021-11-19 11:03:36 -0500

    Minor fixes

--- phase-3-project-frontend/src/components/About.js
+++ phase-3-project-frontend/src/components/About.js
@@ -1,8 +1,7 @@
 import React from "react";
 function About(){
     return(
-        <div>
-            <h1>hidden</h1>
+        <div class="content">
             <h2>This is an app to help you track all your loans and pay them off at your wishes!</h2>
             <img src="https://media.istockphoto.com/vectors/man-and-genie-giant-in-the-magic-lamp-vector-id667707858?k=20&m=667707858&s=170667a&w=0&h=QnIPL_LHVGMq5u0awEZGddscqBIGp1hMUJKCGmKAdc8=" alt="genie"></img>
             <h2>This app is made by Adam, Amy and Jane.</h2>
